b74c2e7f5e79752398c31984bd8b17bacc7c62a7
laubernal <lbernalrodrguez@gmail.com>
2021-06-25 16:24:58 +0200

    starting users repository

---
+++ repositories/users.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+class UsersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Creating a repository requires a filename');
+    }
+
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, '[]');
+    }
+  }
+
+  async getAll() {
+    return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
+  }
+
+  async create(attributes) {
+    attributes.id = this.randomID();
+
+    const records = await this.getAll();
+    records.push(attributes);
+
+    await this.writeAll(records);
+  }
+
+  async writeAll(records) {
+    await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+  }
+
+  randomID() {
+    return crypto.randomBytes(4).toString('hex');
+  }
+}
+
+const test = async () => {
+  const repo = new UsersRepository('users.json');
+
+  await repo.create({ email: 'test@test.com', password: 'password' });
+
+  const users = await repo.getAll();
+
+  console.log(users);
+};
+
+test();
